40b2acd9b911a505a2244e4978a50af25ef13d11
akanekwon172 <akanekwon_dev@icloud.com>
2021-09-13 10:18:17 +0900

    Add Todo class

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
 'use strict';
 // { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
-const tasks = [];
+class Todo {
+  constructor(items = []) {
+    this.items = items;
+  }
 
-/**
- * TODOを追加する
- * @param {string} task
- */
-function add(task) {
-  tasks.push({ name: task, state: false });
-}
+  /**
+   * TODOを追加する
+   * @param {string} task
+   */
+  add(task) {
+    this.items = [...this.items, { name: task, state: false }];
+  }
 
-/**
- * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
- * @param {object} taskAndIsDonePair
- * @return {boolean} 完了したかどうか
- */
-function isDone(taskAndIsDonePair) {
-  return taskAndIsDonePair.state;
-}
+  /**
+   * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
+   * @param {object} taskAndIsDonePair
+   * @return {boolean} 完了したかどうか
+   */
+  isDone(taskAndIsDonePair) {
+    return taskAndIsDonePair.state;
+  }
 
-/**
- * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
- * @param {object} taskAndIsDonePair
- * @return {boolean} 完了していないかどうか
- */
-function isNotDone(taskAndIsDonePair) {
-  return !isDone(taskAndIsDonePair);
-}
+  /**
+   * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
+   * @param {object} taskAndIsDonePair
+   * @return {boolean} 完了していないかどうか
+   */
+  isNotDone(taskAndIsDonePair) {
+    return !isDone(taskAndIsDonePair);
+  }
 
-/**
- * TODO一覧の配列を取得する
- * @return {array}
- */
-function list() {
-  return tasks.filter(isNotDone).map(t => t.name);
-}
+  /**
+   * TODOの一覧の配列を取得する
+   * @return {Array} tasks
+   */
+  list() {
+    return this.items?.filter(this.isNotDone).map(t => t.name);
+  }
 
-/**
- * TODOを完了状態にする
- * @param {string} task
- */
-function done(task) {
-  const indexFound = tasks.findIndex(t => t.name === task);
-  if (indexFound !== -1) {
-    tasks[indexFound].state = true;
+  /**
+   * TODOを完了状態にする
+   * @param {string} task
+   */
+  done(task) {
+    const item = this.items?.find(t => t.name === task);
+    const index = this.items?.indexOf(item);
+    this.items[index] = { ...item, state: true };
+  }
+
+  /**
+   * 完了済みのタスクの一覧の配列を取得する
+   * @return {Array} tasks
+   */
+  donelist() {
+    return this.items?.filter(this.isDone).map(t => t.name);
+  }
+
+  /**
+   * 項目を削除する
+   * @param {string} task
+   */
+  del(task) {
+    this.items = this.items?.filter(t => t.name === task);
   }
 }
 
-/**
- * 完了済みのタスクの一覧の配列を取得する
- * @return {Array} tasks
- */
-function donelist() {
-  return tasks.filter(isDone).map(t => t.name);
-}
-
-/**
- * 項目を削除する
- * @param {string} task
- */
-function del(task) {
-  const indexFound = tasks.findIndex(t => t.name === task);
-  if (indexFound !== -1) {
-    tasks.splice(indexFound, 1);
-  }
-}
-
-module.exports = {
-  add,
-  list,
-  done,
-  donelist,
-  del,
-};
+module.exports = Todo;
